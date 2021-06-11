6231a8465199421bcf3653a729f7fc2f3f7e6263
akashadr <75382310+akashadr@users.noreply.github.com>
2021-06-11 16:07:48 +0530

    Add files via upload

---
+++ alban3.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+print(dir(math))
+print(dir(random))
+print(dir())
+
+import builtins
+print(dir(builtins))
+
+from math import sqrt
+
+print(sqrt(9))
+
+#from math import *
+
+from math import pow
+
+print(pow(3,2))
+  
